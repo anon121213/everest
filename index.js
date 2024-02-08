ee497c0a4c0b8f07674a8e74619df415e0062b35
anon121213 <anon121213@mail.ru>
2024-02-08 18:51:28 +0300

    final

--- index.js
+++ index.js
@@ -4,10 +4,77 @@ const tabsContent = document.querySelectorAll('.tab__content');
 const sliderButtonLeft = document.querySelector('.left_button');
 const sliderButtonRight = document.querySelector('.right_button');
 
+const burger = document.querySelector('.burger');
+const mountains = document.querySelector('.mountain');
+const burger_button = document.querySelector('.burger-button');
+const burger_menu = document.querySelector('.burger__menu');
+
+let burger__menu_width = 0;
+let burger__menu_height = 0;
+var mouseX = 0
+var mouseY = 0
+
+
+let windowWidth = window.innerWidth;
+
 let tours = document.querySelector('.tours');
 let childs = tours.childNodes;
 
 let sliderOffset = 0;
+let burgerClick = false;
+let outburgerClick = false;
+
+if(windowWidth > 840){
+    burger.style.display = "none"
+}
+
+
+burger_button.addEventListener('click', event => {
+
+    burgerClick = !burgerClick;
+    console.log(outburgerClick)
+
+    
+    if(burgerClick == true){
+
+        burger_menu.classList.remove('burger-hide');
+
+        burger__menu_width = burger_menu.offsetWidth;
+        burger__menu_height = burger_menu.offsetHeight;
+
+
+    } else if (burgerClick == false){
+
+        burger_menu.classList.add('burger-hide');
+
+    }
+    
+    // document.addEventListener('mousemove', e => {
+
+    //     mouseX = e.clientX
+    //     mouseY = e.clientY
+        
+    //     console.log(burger__menu_width, burger__menu_height)
+    //     console.log(mouseX, mouseY)
+        
+    //     document.addEventListener('click', e =>{
+    //         outburgerClick = true
+            
+    //     })
+
+    //     if (mouseX > burger__menu_width && outburgerClick == true){
+
+    //         burgerClick = false
+    //         outburgerClick = false
+    //         burger_menu.classList.add('burger-hide');
+
+    
+    //     }
+
+    // })
+
+
+})
 
 tabsTitle.forEach(item => item.addEventListener('click', event => {
 
